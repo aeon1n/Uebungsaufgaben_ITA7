3e34a930e7e24973fea5c4487de1f858b1ad8834
Marcel Bialas <marcelbialas@gmail.com>
2025-01-12 18:28:30 +0100

    added my solution to problem week1

---
+++ Week1_Solution/Week1_Solution/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class MarioMore
+{
+    static void Main()
+    {
+        int height;
+
+        do
+        {
+            Console.Write("Height: ");
+            string input = Console.ReadLine()!;
+
+            if (int.TryParse(input, out height) && height >= 1 && height <= 8)
+            {
+                break;
+            }
+        } while (true);
+
+        for (int row = 1; row <= height; row++)
+        {
+            for (int space = 0; space < height - row; space++)
+            {
+                Console.Write(' ');
+            }
+
+            for (int hash = 0; hash < row; hash++)
+            {
+                Console.Write('#');
+            }
+
+            Console.Write("  ");
+
+            for (int hash = 0; hash < row; hash++)
+            {
+                Console.Write('#');
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
